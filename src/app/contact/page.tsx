7cbed60a3dfb6dfeae1df9794ae045407710f4cc
p.velu <p.velu@TFO-BH-MAC016.local>
2024-11-27 11:48:14 +0530

    chore: build issue resolved

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,9 +5,11 @@ import styles from "../../styles/Contact.module.css";
 export default function Contact() {
   return (
     <div className={styles.container}>
-      <h1>Contact Me</h1>
-      <p>If you would like to work together, feel free to reach out!</p>
-      <p>Email: vinod.kumar@email.com</p>
+      <h1 className={styles.header}>Contact Me</h1>
+      <p className={styles.pragraph}>
+        If you would like to work together, feel free to reach out!
+      </p>
+      <p className={styles.pragraph}>Email: vinod.kumar@email.com</p>
     </div>
   );
 }
